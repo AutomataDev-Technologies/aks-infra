488b59c0b19cbcf1d5d2d493ec48a163c6c06e0a
Satya Prakash <spsingh302212@gmail.com>
2025-12-02 01:30:28 +0530

    Update provider.tf

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -4,22 +4,12 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "4.51.0"
     }
-
-    # azuread = {
-    #   source  = "hashicorp/azuread"
-    #   version = "3.7.0"
-    # }
   }
-
-
 }
 
 provider "azurerm" {
   features {}
-
-  subscription_id = "b4461466-1e6b-4be2-bb70-1e96a72a41c8"
+  subscription_id = "e5efa2cd-02a5-4c7b-be99-6e2ae6d5feac"
 }
 
-# provider "azuread" {
-  
-# }
+
