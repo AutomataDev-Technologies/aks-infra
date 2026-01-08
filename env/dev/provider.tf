381d5c1cfaa0a3c764ca0ef32fddfe63def4b2bb
SPSINGH3011 <spsingh302212@gmail.com>
2026-01-08 13:08:16 +0530

    up

--- env/dev/provider.tf
+++ env/dev/provider.tf
@@ -1,16 +1,15 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "4.51.0"
+      source = "hashicorp/azurerm"
+      version = "4.57.0"
     }
-}
+  }
 }
 
 provider "azurerm" {
-  features {}
-
-  subscription_id = "86dc31e5-df0c-4c2c-9818-568d4d373fa6"
+ features {}
+ subscription_id = "86dc31e5-df0c-4c2c-9818-568d4d373fa6"
 }
 
 
